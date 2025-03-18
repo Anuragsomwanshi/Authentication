6b28fb1157f90b6e7162398bf57f4aadc50cc346
Anuragsomwanshi <anuragsomwanshi11@gmail.com>
2025-03-18 18:04:29 +0530

    change

--- Backend/index.js
+++ Backend/index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import cors from "cors"
 import 'dotenv/config';
 import cookieParser from 'cookie-parser';
+import mongoose from 'mongoose';
 
 import AuthRoutes from './Routes/Auth.Routes.js'
 import userRouter from './Routes/User.Routes.js';
